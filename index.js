fe320719a2624ef0e01088ffbc5f1d4c7aafd527
Todd <nexxgodd@gmail.com>
2021-09-21 09:28:57 -0500

    strawbery cake

--- index.js
+++ index.js
@@ -7,21 +7,37 @@ function formatRecipe(){
 	} //escape small list
 	let title = lines.shift();
 	let title2 = title.replace(/\s|’|\./g, "");
-	let direction = lines.pop();
+	let direction = lines.pop().replace(/\.\s+/g,".\n\t\t\t</p>\n\t\t\t<p>\n\t\t\t\t");
 	
 	let ingout='';
 	lines.forEach(i=>ingout+=`\n\t\t\t\t<li class="col-6 col-md-4">\n\t\t\t\t\t${i}\n\t\t\t\t</li>`);
 	
 	//long template string
-	let out=`<div class="accordion-item">\n\t<h2 class="accordion-header" id="heading${title2}">\n\t\t<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${title2}" aria-expanded="false" aria-controls="collapse${title2}">\n\t\t\t${title}\n\t\t</button>\n\t</h2>\n\t<div id="collapse${title2}" class="accordion-collapse collapse" aria-labelledby="heading${title2}" data-bs-parent="#accordion__page_">\n\t\t<div class="accordion-body">\n\t\t\t<ul class="row ">${ingout}\n\t\t\t</ul>\n\t\t\t<hr>\n\t\t\t<p>\n\t\t\t\t${direction}\n\t\t\t</p>\n\t\t</div>\n\t</div>\n</div>`;
+	let out=`<!-- ${title} -->
+<div class="accordion-item">\n\t<h2 class="accordion-header" id="heading${title2}">
+\t\t<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${title2}" aria-expanded="false" aria-controls="collapse${title2}">
+\t\t\t${title}
+\t\t</button>
+\t</h2>
+\t<div id="collapse${title2}" class="accordion-collapse collapse" aria-labelledby="heading${title2}" data-bs-parent="#accordion__page_">
+\t\t<div class="accordion-body">
+\t\t\t<ul class="row ">${ingout}
+\t\t\t</ul>
+\t\t\t<hr>
+\t\t\t<p>
+\t\t\t\t${direction}
+\t\t\t</p>
+\t\t</div>
+\t</div>
+</div>`;
 	
 	document.getElementById("recOutput").value=out;
 	copyOutput();
 	inbox.value="";
 }
 
-function copyOutput() {
-	document.getElementById("recOutput").select();
+function copyOutput(id) {
+	document.getElementById(id).select();
 	document.execCommand("copy");
 } 
 
@@ -92,6 +108,75 @@ function buildArr2(){
 
 function buildArr(){
 	arr=document.getElementById("bingoInput").value.split("\n");
+}
+
+
+function formatFish(){
+	let regex = /"([^\t]+)\n([^\t]+)"/g;
+	let type="";
+	document.getElementsByName('accreature').forEach(r=>{if(r.checked)type= r.value});
+	console.log(type);
+	let lines=document.getElementById("acInput").value.replace(regex,"$1"+" & "+"$2").split("\n");
+	lines=lines.map(row=>{
+		row=row.split('\t');
+		let time=row[type=="bug"?5:6];
+		let obj= {
+			_id:parseInt(row[0]),
+			//1 was checkmark
+			name:row[2].replace(/\s(\w)/g,(letter)=>letter.toUpperCase()),
+			price:parseInt(row[3].replace(',','')),
+			time:time,
+			timeArray:[],
+			months:[]
+		}
+
+
+		if(type=="sea"){
+			obj.swimPattern=row[5];
+			obj.size=row[4];
+		}
+		else{
+			obj.location=row[4];
+		}
+		if(type=="fish"){
+			obj.size=row[5];
+		}
+		
+
+
+
+		
+		if(time=="All day"){
+			obj.timeArray.push({start:0,end:24})
+		}
+		else{
+			for(let t of time.split(" & ")){
+				t=t.split(" ");
+				let start=parseInt(t[0])+(t[1]=="PM"?12:0);
+				let end=parseInt(t[3])+(t[4]=="PM"?12:0);
+				if(start<end){
+					obj.timeArray.push({start:start,end:end});
+				}
+				else{
+					obj.timeArray.push({start:0,end:end});
+					obj.timeArray.push({start:start,end:24});
+				}
+			}
+			obj.timeArray.sort((a,b)=>{return a.start-b.start})
+		}
+		let offset=row.length-12;
+		for(i=offset;i<row.length;i++){
+			obj.months.push(row[i]=="✓");
+		}
+
+		return obj;
+		//return row;
+		 
+	})
+
+	console.log(lines);
+	document.getElementById("acOutput").value=JSON.stringify(lines);
+	//copyOutput();
 }
 
 
